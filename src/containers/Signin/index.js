15c6171fd4244d88db3ba6cc32cdc3f1d6ae46ee
siddharth <sid9057819561@gmail.com>
2021-06-10 19:27:12 +0530

    redux store setup

--- src/containers/Signin/index.js
+++ src/containers/Signin/index.js
@@ -2,15 +2,30 @@ import React from 'react'
 import { Col, Container, Row,Form,Button } from 'react-bootstrap'
 import Layout from '../../components/Layout'
 import Input from "../../components/UI/Input"
+import {login} from "../../actions"
+import {useDispatch} from 'react-redux'
 
 function Signin() {
+   const dispatch=useDispatch();
+  const  userLogin=(e)=>{
+    e.preventDefault();
+    const user={
+    email:"sid9057819561@gmail.com",
+    password:"Sid12345"
+    }
+    dispatch(login(user));
+  }
+
+
+
+
     return (
      <Layout>
         <Container>
             <Row style={{marginTop:"50px"}}>
               <Col md={{span:6, offset:3}}>
 
-                <Form>
+                <Form onSubmit={userLogin}>
                     <Input
                           label="Email"
                           type="email"
